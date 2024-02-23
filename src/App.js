4d082574535ad1073f4655e8e4bf739530675b53
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-02-23 19:25:38 +0330

    fix : Fix routes shopcatecory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Shop from "./pages/Shop";
-import ShopCaregory from "./pages/ShopCaregory";
+import ShopCategory from "./pages/ShopCategory";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import LoginSingup from "./pages/LoginSingup";
@@ -11,9 +11,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCaregory category="men" />} />
-          <Route path="/womens" element={<ShopCaregory category="women" />} />
-          <Route path="/kids" element={<ShopCaregory category="kid" />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/womens" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
