970547dd199c5a98de13f09809c05573c5fa41ec
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-02-24 18:38:45 +0330

    Feat : Delete Navbar and Footer camponent

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,22 +1,18 @@
 import React from "react";
-import Navbar from "../Camponents/Navbar/Navbar";
 import Hero from "../Camponents/Hero/Hero";
 import Popular from "../Camponents/Popular/Popular";
 import Offers from "../Camponents/Offers/Offers";
 import NewCollections from "../Camponents/NewCollections/NewCollections";
 import NewsLatter from "../Camponents/NewsLatter/NewsLatter";
-import Footer from "../Camponents/Footer/Footer";
 
 function Shop() {
   return (
     <div>
-      <Navbar />
       <Hero />
       <Popular />
       <Offers />
       <NewCollections />
       <NewsLatter />
-      <Footer />
     </div>
   );
 }
