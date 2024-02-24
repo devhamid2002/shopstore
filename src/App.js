ac55c1164b5409b61c45344c2bcb061881b4eac6
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-02-24 18:42:13 +0330

    Feat : Add Navbar and Footer Camponent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Camponents/Navbar/Navbar";
 import Shop from "./pages/Shop";
 import ShopCategory from "./pages/ShopCategory";
 import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import LoginSingup from "./pages/LoginSingup";
+import men_banner from "./Camponents/Assets/banner_mens.png";
+import women_banner from "./Camponents/Assets/banner_women.png";
+import kid_banner from "./Camponents/Assets/banner_kids.png";
+import Footer from "./Camponents/Footer/Footer";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="men" />} />
-          <Route path="/womens" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kid_banner} category="kid" />}
+          />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSingup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
