3ee97c775478596824589b97a9dd6d47ba2af8fd
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-02-24 18:38:06 +0330

    Feat : Add JSX Product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrums from "../Camponents/Breadcrums/Breadcrums";
 
 function Product() {
-  return <div></div>;
+  const { all_product } = useContext(ShopContext);
+  const { ProductId } = useParams();
+  const product = all_product.find((e) => e.id === Number(ProductId));
+  return (
+    <div>
+      <Breadcrums product={product} />
+    </div>
+  );
 }
 
 export default Product;
