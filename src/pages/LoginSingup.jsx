68ee7ec6110156315dbf09cff2e7f6bc00d07c39
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-02-24 18:37:32 +0330

    feat : Add JSX LoginSinggup

--- src/pages/LoginSingup.jsx
+++ src/pages/LoginSingup.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import "./CSS/LoginSingup.css";
 function LoginSingup() {
-  return <div></div>;
+  return (
+    <div className="loginsingup">
+      <div className="loginsingup-container">
+        <h1>Sing Up</h1>
+        <div className="loginsingup-fields">
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsingup-login">
+          Alread have an account ? <span>Login here</span>
+        </p>
+        <div className="loginsingup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginSingup;
