818f19d05ad9db136ffd33146f937a2651006383
Hamidreza Yavarzadeh <hamid.yavarzadeh2002@gmail.com>
2024-02-28 20:26:06 +0330

    feat : Add other parts of product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,16 +1,21 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrums from "../Camponents/Breadcrums/Breadcrums";
+import ProductDisplay from "../Camponents/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Camponents/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Camponents/RelatedProducts/RelatedProducts";
 
 function Product() {
   const { all_product } = useContext(ShopContext);
-  const { ProductId } = useParams();
-  const product = all_product.find((e) => e.id === Number(ProductId));
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
   return (
     <div>
       <Breadcrums product={product} />
+      <ProductDisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts />
     </div>
   );
 }
